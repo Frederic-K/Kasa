9b81566234fd14efa6b14f78ac8ae7c04750c9de
Frederic.K <frederic.kreuder@gmail.com>
2023-05-04 17:07:22 +0200

    Update Gallery nd card

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,42 +1,18 @@
-//import Card from '../Card/Card'
+import Card from '../Card/Card'
 
-// function Gallery({ data }) {
-//   return (
-//     <main>
-//       <section className="gallery">
-//         <ul className="gallery__thumb">
-//           {data.map((accomodation) => (
-//             <Card key={accomodation.id} accomodation={accomodation} />
-//           ))}
-//         </ul>
-//       </section>
-//     </main>
-//   )
-// }
+function Gallery({ data }) {
+  return (
+    <section className="main__gallery--cards">
+      {data.map((card) => (
+        <Card
+          key={`${card.id}`}
+          id={`${card.id}`}
+          cover={card.cover}
+          title={card.title}
+        />
+      ))}
+    </section>
+  )
+}
 
-// export default Gallery
-
-// import { NavLink } from 'react-router-dom'
-
-// function Gallery({ data }) {
-//   return (
-//     <ul className="gallery">
-//       {data.map((card) => (
-//         <NavLink to={'/accomodation/' + card.id} key={card.id}>
-//           <li className="gallery__card">
-//             <img
-//               className="gallery__card--cover"
-//               src={card.cover}
-//               alt={card.title}
-//             />
-//             <div className="gallery__card--title">
-//               <h2>{card.title}</h2>
-//             </div>
-//           </li>
-//         </NavLink>
-//       ))}
-//     </ul>
-//   )
-// }
-
-// export default Gallery
+export default Gallery
