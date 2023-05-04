673b0e58cfa58ebaabddf965ff40c7f6f3bbe0bb
Frederic.K <frederic.kreuder@gmail.com>
2023-05-04 12:42:56 +0200

    Log to find error from fetch

--- src/utils/hooks/getDataAccomodation.jsx
+++ src/utils/hooks/getDataAccomodation.jsx
@@ -2,6 +2,9 @@ import useFetch from './getData'
 
 function useGetAccomodationData() {
   const { isLoading, data, error } = useFetch('../../data/logements.json')
+  console.log('error', error)
+  console.log('isLoading', isLoading)
+  console.log('data', data)
   return { isLoading, data, error }
 }
 
