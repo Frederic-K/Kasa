3fceb3f4deec09abb32f579d42974e1f23a152de
Frederic.K <frederic.kreuder@gmail.com>
2023-05-05 10:21:23 +0200

    Set dropdown coponent (without css)

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import arrowUp from '../../assets/icons/arrowUp.svg'
+import arrowDown from '../../assets/icons/arrowDown.svg'
+
+function Dropdown({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const onToggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div className="dropdown">
+      <h3 className="dropdown__title">{title}</h3>
+      <button
+        type="button"
+        className="dropdown__btn"
+        data-status={isOpen ? 'close' : 'open'}
+        onClick={onToggle}
+      >
+        <img
+          className={
+            isOpen ? 'dropdown__btn--arrowUp' : 'dropdown__btn--arrowDown'
+          }
+          src={arrowDown}
+          alt="icone flèche"
+        />
+      </button>
+      <div
+        className={
+          isOpen ? 'dropdown__content--open' : 'dropdown__content--close'
+        }
+      >
+        {typeof content !== 'object' ? (
+          <div>{content}</div>
+        ) : (
+          <ul>
+            {content.map((el, index) => (
+              <li key={`${el}-${index}`}>{el}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Dropdown
