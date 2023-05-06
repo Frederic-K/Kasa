de22467a29c1cb4a0a629396677e63eb9bc8b6fd
Frederic.K <frederic.kreuder@gmail.com>
2023-05-06 13:42:59 +0200

    Start card css

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,15 +2,17 @@ import Card from '../Card/Card'
 
 function Gallery({ data }) {
   return (
-    <section className="main__gallery--cards">
-      {data.map((card) => (
-        <Card
-          key={`card-${card.id}`}
-          id={`${card.id}`}
-          cover={card.cover}
-          title={card.title}
-        />
-      ))}
+    <section className="main__gallery">
+      <div className="main__gallery--cards">
+        {data.map((card) => (
+          <Card
+            key={`card-${card.id}`}
+            id={`${card.id}`}
+            cover={card.cover}
+            title={card.title}
+          />
+        ))}
+      </div>
     </section>
   )
 }
