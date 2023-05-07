46f4988386807d5e23990ca29d17ba532d779608
Frederic.K <frederic.kreuder@gmail.com>
2023-05-07 19:02:41 +0200

    Fix css issues

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,18 +2,16 @@ import Card from '../Card/Card'
 
 function Gallery({ data }) {
   return (
-    <section className="main__gallery">
-      <div className="main__gallery--cards">
-        {data.map((card) => (
-          <Card
-            key={`card-${card.id}`}
-            id={`${card.id}`}
-            cover={card.cover}
-            title={card.title}
-          />
-        ))}
-      </div>
-    </section>
+    <div className="main__gallery--cards">
+      {data.map((card) => (
+        <Card
+          key={`card-${card.id}`}
+          id={`${card.id}`}
+          cover={card.cover}
+          title={card.title}
+        />
+      ))}
+    </div>
   )
 }
 
