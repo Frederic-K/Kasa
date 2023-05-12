c7b13945dc6a31ffe33b82b14c4e4a62a6076afa
Frederic.K <frederic.kreuder@gmail.com>
2023-05-12 08:17:19 +0200

    Rename const Rating as in OC lesson

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -2,13 +2,13 @@ import FullStar from '../../assets/icons/starFull.svg'
 import EmptyStar from '../../assets/icons/starEmpty.svg'
 
 function Rating({ data }) {
-  const rank = [1, 2, 3, 4, 5]
+  const range = [1, 2, 3, 4, 5]
   const rating = data
 
   return (
     <div className="rating__container">
-      {rank.map((rangeLevel, index) =>
-        rating >= rangeLevel ? (
+      {range.map((rangeElem, index) =>
+        rating >= rangeElem ? (
           <div className="rating__icon--fullStar">
             <img src={FullStar} alt="Etoile pleine" key={`fullStar-${index}`} />
           </div>
